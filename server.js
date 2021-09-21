59b609cee7cecef11efd3b3cd5c33f01834234dc
lusifer2644A <64474004+lusifer2644A@users.noreply.github.com>
2021-09-21 13:11:22 +0530

    Final Commit

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require("express");
-
+const path = require("path");
 const connectDB = require("./config/db");
-
 const app = express();
+
+//Connect Database
 connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send(`API RUNNING ON PORT ${PORT}`);
-});
 
 //Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/static"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+//Port
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`DevBook running on port ${PORT}`);
