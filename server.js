a1e399bebfa24500816227a22942d47e37c9df0e
Ankit Kumar Gupta <64474004+lusifer2644A@users.noreply.github.com>
2021-09-21 20:57:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("client/static"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
